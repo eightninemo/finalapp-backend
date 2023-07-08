48ea0adf42edc96beae87dc53ec70d316fc6e012
msooreoluwa <okeyodesamuel@gmail.com>
2023-07-08 10:00:27 +0100

    added location 14

--- routes/location_route.js
+++ routes/location_route.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const LocationController = require('../controllers/location_controller')
 
 
-router.post('/all', LocationController.all)
-router.post('/show/:locationId', LocationController.one)
+router.get('/all', LocationController.all)
+router.get('/show/:locationId', LocationController.one)
 
 
 
