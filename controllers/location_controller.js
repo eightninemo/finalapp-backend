64e8dc6766214b9b8ac65ed30eac21ac6c37e875
msooreoluwa <okeyodesamuel@gmail.com>
2023-07-08 10:13:16 +0100

    added location 17

--- controllers/location_controller.js
+++ controllers/location_controller.js
@@ -17,14 +17,19 @@ const all = (req, res) => {
 // get cafetaria by ID
 const one = (req, res) => {
     const locationName = req.params.locationName
-    Location.findOne({location_name: locationName})
-    .then(response => {
+    Location.findOne({location_name: locationName}, function(err,response){
+        if (err) {
+            res.status(200).json({
+                message: err
+            })
+          }
+          if (!response) {
+            res.status(200).json({
+                message: 'Location not found.'
+            })
+          }
         res.status(200).json({
             data: response
-        })
-    }).catch(error => {
-        res.status(!200).json({
-            message: 'An error occured'
         })
     })
 }
