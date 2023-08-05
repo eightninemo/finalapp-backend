69bf25f18d0ef4173760d07a103295058483244b
msooreoluwa <okeyodesamuel@gmail.com>
2023-08-05 11:56:15 +0100

    'user' to 'patient'

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -7,7 +7,7 @@ const DoctorController = require('../controllers/doctor_controller')
 
 router.post('/patient-register', PatientController.register)
 router.post('/doctor-register', DoctorController.register)
-router.post('/user-login', PatientController.login)
+router.post('/patient-login', PatientController.login)
 router.post('/doctor-login', DoctorController.login)
 
 
